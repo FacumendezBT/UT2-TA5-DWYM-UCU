aaa2979dc59cfd5eafe3a806fb96e09b52d5c536
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-14 10:00:58 -0300

    UT2-TA5

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('btn');
+const inputAnio = document.getElementById('input');
+
+btn.addEventListener('click', function () {
+    let anio = input.value;
+    leapYears(anio);
+});
+
+/**
+ * Si el numero pasado por parametro corresponde a un año bisiesto, imprime true, de lo contrario imprime false.
+ * Un año bisiesto es aquel que es divisible entre 4, o si es divisible entre 100 y también es divisible entre 400.
+ * @param {number} anio - Año a evaluar.
+ */
+function leapYears(anio) {
+    if (anio % 4 === 0 || (anio % 100 === 0 && anio % 400 === 0)) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
