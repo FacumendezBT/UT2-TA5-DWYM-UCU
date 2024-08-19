f5763ce654328d3c1c8068e1f0a0dd90f02a1a53
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-19 08:59:08 -0300

    onload por las dudas

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const btn = document.getElementById('btn');
-const inputAnio = document.getElementById('input');
+window.onload = function () {
+    const btn = document.getElementById('btn');
+    const inputAnio = document.getElementById('input');
 
-btn.addEventListener('click', function () {
-    let anio = input.value;
-    leapYears(anio);
-});
-
+    btn.addEventListener('click', function () {
+        let anio = inputAnio.value;
+        leapYears(anio);
+    });
+}
 /**
  * Si el numero pasado por parametro corresponde a un año bisiesto, imprime true, de lo contrario imprime false.
  * Un año bisiesto es aquel que es divisible entre 4, o si es divisible entre 100 y también es divisible entre 400.
